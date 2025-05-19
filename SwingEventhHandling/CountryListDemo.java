cb7e0c7c30b927135d98b158372cd5638e9c92a3
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 20:03:22 +0530

    Update CountryListDemo.java

--- SwingEventhHandling/CountryListDemo.java
+++ SwingEventhHandling/CountryListDemo.java
@@ -1,3 +1,6 @@
+/*5a. Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada, Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and
+display them on console whenever the countries are selected on the list.*/
+
 package program_5;
 
 import java.util.List;
