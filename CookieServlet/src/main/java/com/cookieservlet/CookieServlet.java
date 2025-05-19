bc4e18a96c174b9edc98c68e1d9410e7477ad402
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 19:07:51 +0530

    Update CookieServlet.java

--- CookieServlet/src/main/java/com/cookieservlet/CookieServlet.java
+++ CookieServlet/src/main/java/com/cookieservlet/CookieServlet.java
@@ -1,3 +1,6 @@
+/*6b. Build a servlet program to create a cookie to get your name through text box and press submit button( through HTML) to display the message by greeting Welcome back your name ! , you have
+visited this page n times ( n = number of your visit ) and demonstrate the expiry of cookie also.*/
+
 package com.cookieservlet;
 //Step 1: Required imports
 import java.io.*;
