85631f5bd208ca1561eef953192531900e47b951
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 12:16:07 +0530

    Update ColorSubListDemo.java

--- ListOperations/ColorSubListDemo.java
+++ ListOperations/ColorSubListDemo.java
@@ -1,3 +1,5 @@
+/*1b. Array List programs
+5. Write a java program for getting different colors through ArrayList interface and extract the elements 1st and 2nd from the ArrayList object by using SubList()*/
 package Program_1;
 import java.util.ArrayList;
 import java.util.List;
