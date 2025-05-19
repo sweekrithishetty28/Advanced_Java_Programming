56dda447c97762a55e70a05d7a4816e72345427d
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 19:51:07 +0530

    Update ButtonClickDemo.java

--- SwingEventhHandling/ButtonClickDemo.java
+++ SwingEventhHandling/ButtonClickDemo.java
@@ -1,3 +1,5 @@
+/*4b. Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+event handling mechanism with addActionListener( ).*/
 package program_4;
 
 import java.awt.Font;
