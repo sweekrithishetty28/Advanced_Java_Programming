8826dee61832ff582c1b2f9a0cb2f2195d73acbd
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 19:44:52 +0530

    Update StringFunctionsDemo.java

--- StringOperationAndManipulation/StringFunctionsDemo.java
+++ StringOperationAndManipulation/StringFunctionsDemo.java
@@ -1,3 +1,8 @@
+/* 3a. Develop a java program for performing various string operations with different string handling functions directed as follows:
+String Creation and Basic Operations, Length and Character Access, String Comparison, String Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and startsWith() and endsWith() */
+
+
 package program_3;
 import java.util.Arrays;
 public class StringFunctionsDemo {
