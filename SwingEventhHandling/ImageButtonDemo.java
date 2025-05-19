d52bcaa998ccd29254d843460cb56c7279f0dda8
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 20:04:07 +0530

    Update ImageButtonDemo.java

--- SwingEventhHandling/ImageButtonDemo.java
+++ SwingEventhHandling/ImageButtonDemo.java
@@ -1,3 +1,6 @@
+/*4c. Develop a Swing program in Java to display a message “Digital Clock is pressed” or “Hour Glass is pressed” depending upon the Jbutton with image either Digital Clock or Hour Glass is
+pressed by implementing the event handling mechanism with addActionListener( ).*/
+
 package program_4;
 
 import java.awt.Font;
