c30e6a1fa132f5e86db86423536862d074a10cfa
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-19 19:09:04 +0530

    Update FactorialServlet.java

--- FactorialServlet/src/main/java/com/factorial/FactorialServlet.java
+++ FactorialServlet/src/main/java/com/factorial/FactorialServlet.java
@@ -1,3 +1,5 @@
+/*6a. Build a servlet program to find the factorial of a number using HTML with step by step procedure.*/
+
 package com.factorial;
 import java.io.IOException;
 import java.io.PrintWriter;
