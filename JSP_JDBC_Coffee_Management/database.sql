b2594599058f0a2aa47efc908339d3a6433e4403
sweekrithishetty28 <sweekrithishetty1@gmail.com>
2025-05-21 22:38:22 +0530

    inital commit

--- JSP_JDBC_Coffee_Management/database.sql
+++ JSP_JDBC_Coffee_Management/database.sql
@@ -5,9 +5,13 @@ USE test;
 CREATE TABLE IF NOT EXISTS coffee (
     id INT PRIMARY KEY,
     coffee_name VARCHAR(100),
-    price DECIMAL(10, 2)
+    price int
 );
 
-INSERT INTO coffee VALUES (1, 'Dark Roast', 100.00);
-INSERT INTO coffee VALUES (2, 'Doppio', 120.00);
-INSERT INTO coffee VALUES (3, 'Espresso', 90.00);
+INSERT INTO coffee VALUES (1, 'Febary', 120);
+INSERT INTO coffee VALUES (2, 'Royal', 190);
+INSERT INTO coffee VALUES (2, 'Diamond', 180);
+INSERT INTO coffee VALUES (3, 'Espresso', 900);
+INSERT INTO coffee VALUES (2, 'Doppio', 120);
+INSERT INTO coffee VALUES (2, 'Drip Coffee',80);
+INSERT INTO coffee VALUES (2, 'Deaf Latte', 110);
